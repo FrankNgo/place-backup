e0ef4883d35cb223c3bc76692138f8ecfcb25eb0
Guest User <Guest@Epicodus-4C.local>
2018-04-09 16:39:22 -0700

    get ready for saving with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'app';
 
   color = 'black';
 
+  drawnPixels = [];
+
+  ngOnInit() {
+    var canvas = <HTMLCanvasElement> document.getElementById("grid");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    // console.log(this.drawnPixels);
+    // ctx.fillRect(((Math.ceil((100)/15)*15)-15),(((Math.ceil((100)/15)*15)-15)),15,15);
+  }
+
   addSquare($event){
-    console.log($event);
-    var canvasElementId = 'grid';
-    var canvasSetup = document.getElementById("grid");
-    var text = canvasSetup;
+    // console.log($event);
     var canvas = <HTMLCanvasElement> document.getElementById("grid");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = this.color;
-    ctx.fillRect(((Math.ceil(($event.offsetX)/15)*15)-15),(((Math.ceil(($event.offsetY)/15)*15)-15)),15,15);
+    var colors = this.color;
+    var x = (Math.ceil(($event.offsetX)/15)*15)-15;
+    var y = (Math.ceil(($event.offsetY)/15)*15)-15;
+    ctx.fillRect(x,y,15,15);
+    this.drawnPixels.push({x,y,colors});
+    console.log(this.drawnPixels);  
   }
 
   setColor(colorset) {
     this.color = colorset;
-    console.log(this.color);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpModule } from '@angular/http';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
