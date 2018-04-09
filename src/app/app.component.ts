d871b85e8027672d2626963b896383c10fa8677e
Guest User <Guest@Epicodus-4C.local>
2018-04-09 15:37:17 -0700

    overwrite frank's messup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
 
+  color = 'black';
 
-
-  storeGuess($event){
+  addSquare($event){
     console.log($event);
+    var canvasElementId = 'grid';
+    var canvasSetup = document.getElementById("grid");
+    var text = canvasSetup;
+    var canvas = <HTMLCanvasElement> document.getElementById("grid");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = this.color;
+    ctx.fillRect(((Math.ceil(($event.screenX-593)/15)*15)-15),(((Math.ceil(($event.screenY-115)/15)*15)-15)),15,15);
+  }
+
+  setColor(colorset) {
+    this.color = colorset;
+    console.log(this.color);
   }
 
 }
